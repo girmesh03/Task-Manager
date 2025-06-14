99ef65af48af1bee8851898c1e5008e8146db2ca
girmesh03 <girmachewzewdie03@gmail.com>
2025-06-14 19:45:02 +0300

    initial commit: project setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
